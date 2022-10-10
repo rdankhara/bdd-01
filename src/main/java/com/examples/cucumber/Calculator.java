dcc53a17d685782ac881179d67496dbdec715e93
rajnikant <rajnikant.dankhara@gmail.com>
2022-10-10 19:49:58 +0100

    added lombok dependency

--- src/main/java/com/examples/cucumber/Calculator.java
+++ src/main/java/com/examples/cucumber/Calculator.java
@@ -1,7 +1,11 @@
 package com.examples.cucumber;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j()
 public class Calculator {
     public int add(int a, int b) {
+        log.info("a:{} and b:{}", a, b);
         return a + b;
     }
 }
