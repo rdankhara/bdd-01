682ee7bf7e3c6cc093675373139532ed7dc5e9bd
rajnikant dankhara <rajnikant.dankhara@gmail.com>
2024-03-09 09:51:22 +0000

    cleaned up pom.xml

--- src/main/java/com/examples/cucumber/App.java
+++ src/main/java/com/examples/cucumber/App.java
@@ -2,26 +2,24 @@ package com.examples.cucumber;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.ILoggerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 // Simple Logging Facade for Java
 @Slf4j
 public class App {
-
     public static void checker(@NonNull String input) {
         log.info("input string is:{}", input);
     }
 
     public static void main(String[] args) {
         Person person = new Person();
-
         person.setFirstName("Abc");
         person.setLastName("Bcd");
-
+        Marker marker = MarkerFactory.getMarker(App.class.getName());
+        log.trace(marker, "abc {}", person.getFirstName());
         log.info("java.class.path:{}", System.getProperty("java.class.path"));
-        log.info("FirstName: {} LastName: {}", person.getFirstName(), person.getLastName());
+        log.debug("FirstName: {} LastName: {}", person.getFirstName(), person.getLastName());
         try {
             checker(null);;
         } catch(Exception e) {
